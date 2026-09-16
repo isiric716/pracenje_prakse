51d7634b2864db55002d21505d7bed261689dff0
Ivona.Siric <Ivona.Siric@be-terna.com>
2026-09-16 12:13:47 +0200

    registracija ne radi

--- internship-tracker/backend/server.js
+++ internship-tracker/backend/server.js
@@ -53,6 +53,31 @@ function authenticateToken(req, res, next) {
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:3001",
+  ];
+
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  } else if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 const currentInternshipStatus = db.prepare(`
   SELECT status
   FROM internships
